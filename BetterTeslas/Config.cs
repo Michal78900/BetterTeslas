af2c7bf78de66e52cb9a917a3219bddabcf5d275
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2020-12-13 20:09:57 +0100

    Add files via upload

--- BetterTeslas/Config.cs
+++ BetterTeslas/Config.cs
@@ -13,7 +13,7 @@ namespace BetterTeslas
         [Description("Maximum value for smaller or larger damage amount, ignored if damage from the list is equal to 0 (Damage from the list +/- from 0 to this value)")]
         public int RandomDamageMaxValue { get; set; } = 20;
         
-        [Description("SCPs list with damage amount which tesla will inflict to them (Rerember that one Tesla burst hits 2 or 3 times)")]
+        [Description("SCPs list with damage amount which tesla will inflict to them (Remember that one Tesla burst hits 2 or 3 times)")]
         public Dictionary<RoleType, float> ScpDamageAmount { get; set; } = new Dictionary<RoleType, float>
         {
             {RoleType.Scp049, 425f },
